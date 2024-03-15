084fa67aad75ee42431ba48355ecb898bee31d4f
Wesley Walker <wpwalker1@gmail.com>
2024-03-15 13:35:42 -0500

    V1 test

---
+++ def draw_face():.py
@@ -0,0 +1,12 @@
+def draw_face():
+    print("   00000   ")
+    print(" 0       0 ")
+    print("0   0 0   0")
+    print("0    0    0")
+    print(" 0 ----- 0 ")
+    print("  0  ___  0")
+    print("    0   0  ")
+    print("    0   0  ")
+
+if __name__ == "__main__":
+    draw_face()
